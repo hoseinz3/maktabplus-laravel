16a94e7ce4c068d3edecc09daf5712f494ba55d9
hoseinz3 <mhs.abedi@gmail.com>
2018-04-02 09:48:38 +0430

    3_7-Blade

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>
         {{$product->name}}
     </h1>
@@ -15,6 +8,4 @@
     <p>
         {{$product->description}}
     </p>
-
-</body>
-</html>
+@endsection
